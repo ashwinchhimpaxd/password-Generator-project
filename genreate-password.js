c79e43f8d79d94d5f7ead091fae5388d64d31f25
ashwin <ashwinlala@gmail.com>
2024-07-23 00:18:18 +0530

    add evennts for icons

--- genreate-password.js
+++ genreate-password.js
@@ -1,34 +1,55 @@
-class PasswordClass {
-    #password;
-    Createstrongpass(code) {
-        let valueforpasscode = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
-        let max = valueforpasscode.length;
-        let newcode = ""
-        for (let index = 0; index <= code; index++) {
-            let randomvalue = Math.floor(Math.random() * max);
-            newcode += valueforpasscode[randomvalue]
+const SubmitBtn = document.querySelector('.submit');
+const passwordlist = document.querySelectorAll('.drop-item ul>li')
+const passwordfield = document.querySelector('#passwordField');
+const inputfielvalue = document.querySelector('#userNameField')
+const passwordtoggleBtn = document.querySelector('.passwordicon');
+const form = document.querySelector('form')
+passwordlist.forEach((list) => {
+    list.addEventListener('click', (li) => generatepassword(li))
+})
+
+passwordtoggleBtn.addEventListener('click', () => {
+    let classes = passwordtoggleBtn.classList.contains("fa-eye-slash");
+    if (classes) {
+        passwordtoggleBtn.classList.add('fa-eye')
+        passwordtoggleBtn.classList.remove('fa-eye-slash')
+        passwordfield.type = "text"
+    } else {
+        passwordtoggleBtn.classList.remove('fa-eye')
+        passwordtoggleBtn.classList.add('fa-eye-slash')
+        passwordfield.type = "password"
+    }
+})
+
+function generatepassword(li) {
+    const listvalue = li.target.innerHTML;
+    passwordlist.forEach((listitem) => {
+        if (listitem.innerHTML == listvalue) {
+
+            let password = Createstrongpass()
+            passwordfield.value = password;
+            return;
         }
-        this.#password = newcode;
-        return "password created successfully"
-    }
-    checkstrongCode() {
-        const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*()_+\-=<>?])(.*)$/;
-        if (regex.test(this.#password)) console.log("Strong Password created")
-        else {
-            this.Createstrongpass(10)
-            this.checkstrongCode();
-        }
-    }
-    showpassword() {
-        return this.#password
-    }
+    })
 }
-let user1 = new PasswordClass()
-user1.Createstrongpass(10);
-user1.checkstrongCode();
-let mypassword = user1.showpassword();
-console.log("my password ", mypassword)
-// console.log(user1.#password)  // password is a private is not access directly outside the class but you can access using showpoassword FX 
+SubmitBtn.addEventListener('click', (e) => welcomeMessage(e))
 
+const welcomeMessage = (e) => {
+    e.preventDefault();
+    const welcomeMessage = document.querySelector('.welcomemessage');
+    let inputtext = inputfielvalue.value.trim();
+    welcomeMessage.innerHTML = `Hey ${inputtext}`;
 
-
+}
+function Createstrongpass() {
+    let valueforpasscode = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
+    let max = valueforpasscode.length;
+    let newcode = ""
+    for (index = 0; index <= 12; index++) {
+        let randomvalue = Math.floor(Math.random() * max);
+        newcode += valueforpasscode[randomvalue]
+    }
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*()_+\-=<>?])(.*)$/;
+    if (regex.test(newcode)) return newcode;
+    return Createstrongpass();
+}
