a9a6ebd13f46b2b9b6a2e307105bdea91b22cded
ashwin <ashwinlala@gmail.com>
2024-07-26 23:22:59 +0530

    Revert "setup password critearea crate area"
    This reverts commit 4e0d771efb6ce0dc3470d8ea900f62b717e5d89b.

--- genreate-password.js
+++ genreate-password.js
@@ -40,7 +40,7 @@ const welcomeMessage = (e) => {
         inputfielvalue.value = "";
         passwordfield.value = "";
     } else {
-        alert('Atleast one Upper and lower character and one deigit and special character must')
+        alert('not recommended')
     }
 }
 function Createstrongpass() {
